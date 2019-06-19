aacd03a1749b99a98d3472a9f59b48d7f233f59a
Dopesmoker <Siloda24@gmail.com>
2019-06-20 02:31:22 +0300

    8

--- HahaDota/Program.cs
+++ HahaDota/Program.cs
@@ -1,34 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HahaDota
 {
-    class Hero
+  
+   public class Hero
     {
-        private String name;
-        private int hp;
-        private int strength;
-        private int agility;
-        private int intelligence;
-        private int attak;
+        protected String name;
+        protected int hp;
+        protected int strength;
+        protected int agility;
+        protected int intelligence;
+        protected int attak;
+        protected int strike;
         public Hero()
         {
-            getInfo();
+
         }
-        public Hero(int hp, int strength, int agility, int intelligence, int attak)
+        public virtual int setHitHero (Hero opponent)
         {
-
-            this.hp = hp;
-            this.strength = strength;
-            this.agility = agility;
-            this.intelligence = intelligence;
-            this.attak = attak;
-
+            return  opponent.hp = hp - strike;
         }
+        
         public String getName()
         {
             return name;
         }
-        public int getHp()
+        public virtual int getHp()
         {
             return hp;
         }
@@ -36,7 +34,7 @@ namespace HahaDota
         {
             return strength;
         }
-        public int getAgillity()
+        public int getAgility()
         {
             return agility;
         }
@@ -44,125 +42,196 @@ namespace HahaDota
         {
             return intelligence;
         }
+        public int getAgirlitySrike()
+        {
+            return strike= (agility / 3) + (strength / 5) + (intelligence / 7);
+        }
+        public int getIntelligenceSrike()
+        {
+            return strike = (intelligence / 3) + (agility / 5) + (strength / 7);
+        }
+        public int getStrengthSrike()
+        {
+            return strike = (strength / 3) + (intelligence / 5) + (agility / 7);
+        }
+       
         public virtual String getInfo()
         {
 
-            return "Name: " + name + "hp: " + hp + "strength: " + strength + "agility: " + agility + "intelligence: " + intelligence + "attak: " + attak;
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike;
         }
 
     }
-    class Sniper : Hero
+    
+    public class Pudge : Hero
     {
-        String name = "Sniper";
-        int hp = 580;
-        int strength = 19;
-        int agility = 21;
-        int intelligence = 15;
-        int attak = 10;
+        private String pudgeName = "Pudge";
+        public int pudgeHp = 700;
+        private static int pudgeStrength = 25;
+        private static int pudgeAgility = 14;
+        private static int pudgeIntelligence = 16;
+        private int pudgeStrike;
 
-        public Sniper()
+        public Pudge()
         {
+            name = pudgeName;
+            hp = pudgeHp;
+            strength = pudgeStrength;
+            agility = pudgeAgility;
+            intelligence = pudgeIntelligence;
+            strike = pudgeStrike;
+            getStrengthSrike();
         }
         public override String getInfo()
         {
-            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + attak;
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike;
         }
     }
-    class Pudg : Hero
+    public class Viper : Hero
     {
-        String name = "Pudg";
-        int hp = 700;
-        int strength = 25;
-        int agility = 14;
-        int intelligence = 16;
-        int attak = 10;
-
-        public Pudg()
-        {
-
-        }
-        public override String getInfo()
-        {
-            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + attak;
-        }
-    }
-    class Viper : Hero
-    {
-        String name = "Viper";
-        int hp = 620;
-        int strength = 21;
-        int agility = 21;
-        int intelligence = 15;
-        int attak = 10;
+        private String viperName = "Viper";
+        private int viperHp = 620;
+        private static int viperStrength = 21;
+        private static int viperAgility = 21;
+        private static int viperIntelligence = 15;
+        private int viperStrike;
 
         public Viper()
         {
-
+            name = viperName;
+            hp = viperHp;
+            strength = viperStrength;
+            agility = viperAgility;
+            intelligence = viperIntelligence;
+            strike = viperStrike;
+            getIntelligenceSrike();
         }
+       
         public override String getInfo()
         {
-            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + attak;
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike;
         }
     }
-    class Luna : Hero
+
+    public class Luna : Hero
     {
-        String name = "Luna";
-        int hp = 520;
-        int strength = 16;
-        int agility = 18;
-        int intelligence = 16;
-        int attak = 10;
+        private String lunaName = "Luna";
+        private int lunaHp = 520;
+        private static int lunaStrength = 16;
+        private static int lunaAgility = 18;
+        private static int lunaIntelligence = 16;
+        private int lunaStrike;
 
         public Luna()
         {
-
+            name = lunaName;
+            hp = lunaHp;
+            strength = lunaStrength;
+            agility = lunaAgility;
+            intelligence = lunaIntelligence;
+            strike = lunaStrike;
+            getAgirlitySrike();
         }
         public override String getInfo()
         {
-            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + attak;
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike;
         }
     }
-    class EmberSpirit : Hero
+    public class Lion : Hero
     {
-        String name = "EmberSpirit";
-        int hp = 620;
-        int strength = 21;
-        int agility = 22;
-        int intelligence = 20;
-        int attak = 10;
+        private String lionName = "Lion";
+        private int lionHp = 560;
+        private static int lionStrength = 18;
+        private static int lionAgility = 15;
+        private static int lionIntelligence = 18;
+        private int emberSpiritStrike;
 
-        public EmberSpirit()
+        public Lion()
         {
-
+            name = lionName;
+            hp = lionHp;
+            strength = lionStrength;
+            agility = lionAgility;
+            intelligence = lionIntelligence;
+            strike = emberSpiritStrike;
+            getStrengthSrike();
         }
         public override String getInfo()
         {
-            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + attak;
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike;
+        }
+    }
+    public class Sniper : Hero 
+    {
+        private String sniperName = "Sniper";
+
+        private int sniperHp = 580;
+        private static int sniperStrength =19;
+        private static int sniperAgility=21;
+        private static int sniperIntelligence=15;
+        private int sniperStrike;
+        
+        public Sniper()
+        {
+            name = sniperName;
+            hp = sniperHp;
+            strength = sniperStrength;
+            agility = sniperAgility;
+            intelligence = sniperIntelligence;            
+            strike = sniperStrike;
+            getAgirlitySrike();
+        }
+
+        public override String getInfo()
+        {
+            return "Name: " + name + "\n hp: " + hp + "\n strength: " + strength + "\n agility: " + agility + "\n intelligence: " + intelligence + "\n attak: " + strike; 
         }
     }
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Hero[] hero = new Hero[4];
-                        
-            Sniper sniper = new Sniper();
-            Pudg pudg = new Pudg();
-            Viper viper = new Viper();
-            Luna luna = new Luna();
-            EmberSpirit emberspirit = new EmberSpirit();
+            List<Hero> people = new List<Hero>(4);
+            people.Add(new Sniper());
+            people.Add(new Pudge());
+            people.Add(new Viper());
+            people.Add(new Luna());
+            people.Add(new Lion());
 
-            Console.WriteLine(sniper.getInfo());
-            Console.WriteLine(" ");
-            Console.WriteLine(pudg.getInfo());
-            Console.WriteLine(" ");
-            Console.WriteLine(viper.getInfo());
-            Console.WriteLine(" ");
-            Console.WriteLine(luna.getInfo());
-            Console.WriteLine(" ");
-            Console.WriteLine(emberspirit.getInfo());
+
+            foreach (Hero p in people)
+            {
+                //Console.WriteLine(p.getInfo());
+            }
+
+
+
+
+
+            Sniper sniper = new Sniper();
+            Pudge pudge = new Pudge();
+
+            do
+            {
+                sniper.setHitHero(pudge);
+                pudge.getHp();
+                Console.WriteLine(" ");
+                Console.WriteLine(pudge.getInfo());
+                Console.WriteLine();
+                if (pudge.pudgeHp>0)
+                {
+                    pudge.getHp();
+                }
+
+            }
+            while (pudge.pudgeHp > 0);
+
+            
+
 
         }
+
+       
     }
     
 }
